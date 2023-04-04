07f532d53d88123cde53a03abd5ad6810fdb328b
ezbugatti <rgun.turuu@gmail.com>
2023-04-04 18:45:45 +0800

    axios

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,34 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
+import { elements } from "./view/base";
+import * as searchView from "./view/searchView";
 
-let search = new Search("pasta");
-search.doSearch().then((r) => console.log(r));
+// web app tolov
+// Хайлтын query, үр дүн
+// Тухайн үзүүлж байгаа жорууд
+//Лайкалсан жорууд
+// Захиалж байгаа жорууд
+
+const state = {};
+const controlSearch = async () => {
+  try {
+    // 1. Вэбээс хайлтын түлхүүр үгийг гаргаж авна
+    const query = searchView.getInput();
+
+    if (query) {
+      // 2. Шинэ хайлтын объектийг үүсгэж өгн
+      state.search = new Search(query);
+      // 3. Хайлт хийхэд зориулж дэлгэцийг бэлтгэнэ
+      // 4. Хайлтыг гүйцэтгэнэ
+      await state.search.doSearch();
+      // 5. Хайлтын үр дүн дэлгэцэнд үзүүлнэ.
+      searchView.renderRecipes(state.search.result);
+    }
+  } catch (error) {
+    alert(error);
+  }
+};
+elements.searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
